1c97a909d3fbbeb360b10b0f765150b2ed561aec
Antonio Sotelo <asotel6@gmail.com>
2018-01-16 18:01:33 -0600

    added jumpbox

--- main.tf
+++ main.tf
@@ -211,3 +211,25 @@ resource "azurerm_sql_firewall_rule" "fw" {
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
+
+// Add jumpbox
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = "${azurerm_resource_group.network.name}"
+  location            = "${azurerm_resource_group.network.location}"
+  vm_hostname         = "jumpbox1"
+  admin_password      = "C0mplxP@s$w0rd!"
+  public_ip_dns       = ["jumpbox1"]
+  nb_public_ip        = "1"
+  remote_port         = "3389"
+  nb_instances        = "1"
+  vm_os_publisher     = "MicrosoftWindowsServer"
+  vm_os_offer         = "WindowsServer"
+  vm_os_sku           = "2012-R2-Datacenter"
+  vm_size             = "Standard_DS2_V2"
+  vnet_subnet_id      = "${azurerm_subnet.subnet.id}"
+
+  tags {
+    name = "Antonio Sotelo"
+  }
+}
